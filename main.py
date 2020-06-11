f42e8e0a8e8960ff59e144fc88f37dce3e87ccdf
Jacek <J.Dobrowolski@stud.elka.pw.edu.pl>
2020-06-11 23:20:33 +0200

    calculating error

--- main.py
+++ main.py
@@ -11,32 +11,32 @@ if __name__ == '__main__':
     df.rename(columns={"czas(min)": "time", "alpha": "compressor", "temp": 'ambient_temp', "Ts": 'refrigerator_temp'}, inplace=True)
     df.set_index('time', inplace=True)
 
-
-
-    delay = 4
+    delay = 2
     frames = [df.shift(-t) for t in range(delay)]
     # small_df = df.iloc[:40]
     delayed_df = pd.concat(frames, axis=1).dropna()
-
-
 
     import random_forest
     from random_forest.tree import Tree
     from random_forest.forest import Forest
 
-    random_forest.tree.config.create_config(min_split_size=15, number_of_predictors_to_draw=5)
-    
-    random_forest.forest.forest_config.data_size_for_tree = 150
-    random_forest.forest.forest_config.number_of_trees = 100
-
+    random_forest.tree.config.create_config(min_split_size=5, number_of_predictors_to_draw=3)
+    random_forest.forest.config.create_config(data_size_for_tree=50, number_of_trees=200)
 
     start = time.perf_counter()
     forest = Forest(delayed_df)
     finish = time.perf_counter()
     print(f"Forest created in {finish-start} second(s)")
 
-    print(forest.out_of_bag.shape)
-    test = forest.out_of_bag.iloc[0].values
+    print(f"Out of bag data size {forest.out_of_bag.shape[0]}")
+    prediction_err_sum = 0
+    actual_value = 0
+    naive_err_sum = 0
+    for test_data in forest.out_of_bag.values:
+        actual_value = test_data[-1]
+        prediction_err_sum += abs(forest.predict(test_data[:-1]) - actual_value)/actual_value
+        naive_err_sum =+ abs(test_data[-4] - actual_value)/actual_value
 
-    print(test)
-    print(forest.predict(test))
+    out_of_bag_size = forest.out_of_bag.shape[0]
+    print(f"Out of bag relative error {abs(prediction_err_sum/out_of_bag_size)*100}%")
+    print(f"Naive relative error {abs(naive_err_sum)/out_of_bag_size*100}%")
--- random_forest/forest.py
+++ random_forest/forest.py
@@ -1,13 +1,20 @@
 from random_forest.tree import Tree
 import numpy as np
+import json
 import concurrent.futures
+
 
 class Config(object):
     def __init__(self):
         self.number_of_trees = 1
         self.data_size_for_tree = 1
+    
+    def create_config(self, number_of_trees, data_size_for_tree):
+        self.number_of_trees = number_of_trees
+        self.data_size_for_tree = data_size_for_tree
 
-forest_config = Config()
+
+config = Config()
 
 
 class Forest(object):
@@ -16,8 +23,8 @@ class Forest(object):
         self.trees = []
         bootstraped_data = []
 
-        for _ in range(forest_config.number_of_trees):
-            training_data = data.sample(forest_config.data_size_for_tree, replace=True)
+        for _ in range(config.number_of_trees):
+            training_data = data.sample(config.data_size_for_tree, replace=True)
             self.out_of_bag = self.out_of_bag[~self.out_of_bag.index.isin(training_data.index)]
             bootstraped_data.append(training_data.values)
 
